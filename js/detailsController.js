4e964d6c71d845658f3f44cceb06565995ad54c6
unknown <rparmar@HARRY-HOTLIPS.aims.gov.au>
2015-12-03 14:05:16 +1000

    template details and bootstrap

--- js/detailsController.js
+++ js/detailsController.js
@@ -4,10 +4,41 @@
 
 angular.module('details', [])
 
-    .controller('detailsController', ['$scope', '$routeParams',
-        function($scope, $routeParams) {
+//    .controller('detailsController', ['$scope', '$routeParams',
+//        function($scope, $routeParams) {
+//            $scope.siteId = $routeParams.id;
+//
+//            console.log($scope.siteId + " Stemplate loaded")
+//            //$scope.phone = data;
+//        }]);
+//
+//;
+
+.controller('detailsController', ['$scope', '$routeParams','$http',
+    function($scope, $routeParams,$http) {
+        $http.get('http://aimsweatherservice.appspot.com/service/vbleachStatuses').success(function(data) {
+
             $scope.siteId = $routeParams.id;
-            console.log($scope.siteId + " Stemplate loaded")
-            //$scope.phone = data;
-        }]);
+            $scope.results = data._embedded.vbleachStatuses;
+
+            for (i = 0; i < $scope.results.length; i++) {
+                if ($scope.results[i].siteId == $scope.siteId) {
+                    $scope.siteDetails = $scope.results[i];
+                    console.log($scope.siteDetails);
+                }
+            }
+
+        });
+
+            //console.log($scope.siteDetails._embedded.vbleachStatuses);
+        //$scope.phone = data;
+    }]);
+
+
+//phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+//    function($scope, $routeParams, $http) {
+//        $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+//            $scope.phone = data;
+//        });
+//    }]);
 ;
