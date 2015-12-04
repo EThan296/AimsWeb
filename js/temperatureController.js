7f2ad4490c233613ec95918c3125b82a07c242db
Ethan Hay <Ehay@AIMS.GOV.AU>
2015-12-04 16:30:00 +1000

    Friday - last commit.  everything working

--- js/temperatureController.js
+++ js/temperatureController.js
@@ -6,178 +6,197 @@ angular.module('site', [])
     $http({method: 'GET', url: 'http://aimsweatherservice.appspot.com/service/vanomolyStatuses'}).success(function(data)
     {
         $scope.anomalyArray = data._embedded.vanomolyStatuses; // response data
+
+        for (var i = 0; i<$scope.anomalyArray.length; i++) {
+            $scope.anomalyArray[i].chartConfig = '';
+
+            var blue = -3 + $scope.anomalyArray[i].plusThreeSd;
+            var lightBlue = $scope.anomalyArray[i].minusThreeSd + $scope.anomalyArray[i].plusTwoSd;
+            var darkGreen = $scope.anomalyArray[i].minusTwoSd + $scope.anomalyArray[i].plusOneSd;
+            var green = $scope.anomalyArray[i].plusOneSd;
+            var green2 = $scope.anomalyArray[i].minusOneSd;
+            var yellow = $scope.anomalyArray[i].plusTwoSd - $scope.anomalyArray[i].plusOneSd;
+            var orange = $scope.anomalyArray[i].plusThreeSd - $scope.anomalyArray[i].plusTwoSd;
+            var red = 3 - $scope.anomalyArray[i].plusThreeSd;
+            var anomaly = $scope.anomalyArray[i].anomoly;
+            $scope.configString = {
+                options: {
+                    chart: {
+                        type: 'bar',
+                        width: 400,
+                        height: 125
+                    },
+                    tooltip: {
+                        enabled: false
+                    },
+                    title: {
+                        text: ''//$scope.anomalyArray[i].siteName
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    subtitle: {
+                        text: ''
+                    },
+                    xAxis: {
+                        categories: ['']
+                    },
+                    yAxis: [{
+                        max:3,
+                        min:-3,
+                        minorTickInterval: 0.25,
+                        title: {
+                            text: ''
+                        }
+                    }, { // mirror axis on right side
+                        opposite: true,
+                        reversed: false,
+                        //categories: categories,
+                        linkedTo: 0,
+                        labels: {
+                            step: 1
+                        }
+                    }],
+                    legend: {
+                        reversed: true,
+                        enabled: false
+                    },
+                    plotOptions: {
+                        column: {
+                            grouping: false
+                        },
+                        series: {
+                            stacking: 'normal',
+                            borderWidth: 0
+                        }
+                    }
+                },
+                series: [
+                    {
+                        name: '6',
+                        color: 'rgba(255, 0, 0, 1)',
+                        data: [red]
+                    }, {
+                        name: '1',
+                        color: 'rgba(0, 0, 255, 1)',
+                        data: [blue]
+                    }, {
+                        name: '2',
+                        color: 'rgba(0, 100, 255, 1)',
+                        data: [lightBlue]
+                    }, {
+                        name: '3',
+                        color: 'rgba(0, 100, 50, 1)',
+                        data: [darkGreen]
+                    }, {
+                        name: '4',
+                        color: 'rgba(0, 238, 0, 1)',
+                        data: [green2]
+
+                    },{
+                        name: '7',
+                        color: 'rgba(283, 118, 0, 1)',
+                        data: [yellow]
+                    },{
+                        name: '5',
+                        color: 'rgba(255, 215, 0, 1)',
+                        data: [orange]
+                    }, {
+                        name: '4',
+                        color: 'rgba(0, 238, 0, 1)',
+                        data: [green]
+
+                    },    {
+                        type: 'scatter',
+                        color: 'black',
+                        data: [anomaly],
+                        marker: {
+                            symbol: 'url(assets/images/rsz_2rsz_1line.png)'
+}
+                    }],
+                loading: false
+            };
+            $scope.anomalyArray[i].chartConfig = $scope.configString;
+        }
     });
 
-    $scope.generateAnomalyChart = function(id, SDone, SDtwo, SDthree, anomaly) {
-      console.log("chart generated");
-        var blue = -3 + SDthree;
-        var lightBlue = (-SDthree) + (SDtwo);
-        var darkGreen = (-SDtwo) + (SDone);
-        var green = SDone;
-        var green2 = -SDone;
-        var yellow = SDtwo - SDone;
-        var orange = SDthree - SDtwo;
-        var red = 3 - SDthree;
-        console.log(blue,lightBlue,darkGreen,green,yellow,orange,red,anomaly)
-
-        $('#anomaly-' + id).highcharts({
-            chart: {
-                type: 'bar'
-            },
-            title: {
-                text: ''
-            },
-            subtitle: {
-                text: ''
-            },
-            xAxis: {
-                categories: ['']
-            },
-            yAxis: [{
-                max:3,
-                min:-3,
-                minorTickInterval: 0.25,
-                title: {
-                    text: ''
-                }
-            }, { // mirror axis on right side
-                opposite: true,
-                reversed: false,
-                //categories: categories,
-                linkedTo: 0,
-                labels: {
-                    step: 1
-                }
-            }],
-            legend: {
-                reversed: true,
-                enabled:false
-            },
-            plotOptions: {
-                column: {
-                    grouping: false
-                },
-                series: {
-                    stacking: 'normal'
-                }
-            },
-            series: [
-                {
-                    name: '1',
-                    color: 'rgba(0, 0, 255, 1)',
-                    data: [blue]
-                }, {
-                    name: '2',
-                    color: 'rgba(0, 100, 255, 1)',
-                    data: [lightBlue]
-                }, {
-                    name: '3',
-                    color: 'rgba(0, 100, 50, 1)',
-                    data: [darkGreen]
-                }, {
-                    name: '4',
-                    color: 'rgba(0, 238, 0, 1)',
-                    data: [green2]
-
-                },{
-                    name: '7',
-                    color: 'rgba(255, 0, 0, 1)',
-                    data: [red]
-
-                }, {
-                    name: '6',
-                    color: 'rgba(283, 118, 0, 1)',
-                    data: [orange]
-                },{
-                    name: '5',
-                    color: 'rgba(255, 215, 0, 1)',
-                    data: [yellow]
-                },{
-                    name: '4',
-                    color: 'rgba(0, 238, 0, 1)',
-                    data: [green]
-
-                },    {
-                    type: 'scatter',
-                    color: 'black',
-                    data: [anomaly],
-                    marker: {
-                        symbol: 'url(http://s22.postimg.org/eavyqgzgt/rsz_2rsz_1line.png)',
-                    }
-            }],
-            credits: { enabled: false }
-
-        });
-    };
-    
+    //Temperature Graphs
     $http.get('http://aimsweatherservice.appspot.com/service/vbleachStatuses')
-
         .then(function(response) {
             $scope.tempArray = response.data._embedded.vbleachStatuses;
-        })
-        $scope.generateChart = function(id, currentTemp, watchTemp, warningTemp, bleachingTemp) {
-        console.log("chart generated");
-        var green = watchTemp;
-        var yellow = warningTemp - watchTemp;
-        var orange = bleachingTemp - warningTemp;
-        var red = 34 - bleachingTemp;
 
-            $('#container-' + id).highcharts({
-                chart: {
-                    type: 'bar'
-                },
-                title: {
-                    text: ''
-                },
-                xAxis: {
-                    categories: ['']
-                },
-                yAxis: {
-                    max:34,
-                    min:20,
-                    minorTickInterval: 0.5,
-                    title: {
-                        text: ''
-                    }
-                },
-                legend: {
-                    reversed: true,
-                    enabled:false
-                },
-                plotOptions: {
-                    column: {
-                        grouping: false
+            for (var i = 0; i < $scope.tempArray.length; i++) {
+                $scope.tempArray[i].chartConfig = '';
+                //console.log($scope.tempArray);
+                var green = $scope.tempArray[i].watchTemp;
+                var yellow = $scope.tempArray[i].warningTemp - $scope.tempArray[i].watchTemp;
+                var orange = $scope.tempArray[i].bleachingTemp - $scope.tempArray[i].warningTemp;
+                var red = 34 - $scope.tempArray[i].bleachingTemp;
+                var currentTemp = $scope.tempArray[i].actualWaterTemp;
+                //console.log(green, yellow, orange, red, currentTemp)
+                $scope.configString2 = {
+                    options: {
+                        chart: {
+                            type: 'bar',
+                            height: 125,
+                            width: 700
+                        },
+                        title: {
+                            text: ''
+                        },
+                        xAxis: {
+                            categories: ['']
+                        },
+                        yAxis: {
+                            max: 34,
+                            min: 24,
+                            minorTickInterval: 0.5,
+                            title: {
+                                text: ''
+                            }
+                        },
+                        legend: {
+                            reversed: true,
+                            enabled: false
+                        },
+                        plotOptions: {
+                            column: {
+                                grouping: false
+                            },
+                            series: {
+                                stacking: 'normal'
+                            }
+                        }
                     },
-                    series: {
-                        stacking: 'normal'
-                    }
-                },
-                series: [{
-                    color: 'rgba(255, 0, 0, 1)',
-                    data: [red]
+                    series: [{
+                        color: 'rgba(255, 0, 0, 1)',
+                        data: [red]
 
-                }, {
-                    color: 'rgba(283, 118, 0, 1)',
-                    data: [orange]
-                }, {
-                    //name: '',
-                    color: 'rgba(255, 215, 0, 1)',
-                    data: [yellow]
-                },  {
-                    name: '',
-                    color: 'rgba(0, 238, 0, 1)',
-                    data: [green]
+                    }, {
+                        color: 'rgba(283, 118, 0, 1)',
+                        data: [orange]
+                    }, {
+                        //name: '',
+                        color: 'rgba(255, 215, 0, 1)',
+                        data: [yellow]
+                    }, {
+                        name: '',
+                        color: 'rgba(0, 238, 0, 1)',
+                        data: [green]
 
-                } , {
-                    type: 'scatter',
-                    color: 'black',
-                    data: [currentTemp],
-                    marker: {
-                        symbol: 'url(http://s22.postimg.org/eavyqgzgt/rsz_2rsz_1line.png)',
-                    }
-                }],
-                credits: { enabled: false }
+                    }, {
+                        type: 'scatter',
+                        color: 'black',
+                        data: [currentTemp],
+                        marker: {
+                            symbol: 'url(assets/images/rsz_2rsz_1line.png)'
+                        }
+                    }],
+                    credits: {enabled: false}
 
-            });
-    };
-})
+                };
+                $scope.tempArray[i].chartConfig = $scope.configString2;
+            }
+        });
+
+});
