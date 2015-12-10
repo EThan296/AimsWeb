a4140bb965d2ac216751c2f3558f4f7543ef05a5
Ethan Hay <Ehay@AIMS.GOV.AU>
2015-12-10 13:34:17 +1000

    service update

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
-angular.module('GBR_Bleaching_Watch', ["ngRoute","highcharts-ng","site","details"])
+angular.module('GBR_Bleaching_Watch', ["ngRoute","highcharts-ng","site","details","bleaching.site","bleaching.siteService"])
 
 .config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
             when('/bleaching', {
                 templateUrl: 'pages/bleaching.html',
-                controller: 'siteCTRL',
+                controller: 'siteCTRL'
             })
 
             .when('/details/:id', {
                 templateUrl: 'pages/singleSiteDetails.html',
-                controller: 'detailsController'
+                //controller: 'detailsController'
+                controller: 'siteController'
             })
 
             .when('/bleaching/historic', {
